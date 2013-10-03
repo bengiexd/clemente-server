4c06aeea9bff9d5811d75534499f9958dabceffa
Bengie Nick <bengiexd@gmail.com>
2013-10-03 17:19:51 -0400

    Update and rename prueva.py to ServerDispositivos.py

---
+++ ServerDispositivos.py
@@ -0,0 +1,57 @@
+import time
+import socket
+import H_Dispositivo
+
+"""
+    Clase servidor
+"""
+class Servidor(H_Dispositivo.Dispositivo):
+    """ 
+        servidor 
+            ip: localhost
+            puerto: 9000
+            nroMaxDispositivos: 100
+    """
+    #ipDispositivos = "192.168.0.102"
+    ipDispositivos = "127.0.0.1"
+    puertoDispositivos = 9000
+    nroMaxDispositivos = 100
+
+    def __init__(self,ip=ipDispositivos,puerto=puertoDispositivos):
+        # socket para atender a los dispositivos remotos        
+        self.ipDispositivos = ip
+        self.puertoDispositivos = puerto
+
+        self.__servidorDispositivos = socket.socket()
+        self.__servidorDispositivos.bind((self.ipDispositivos,self.puertoDispositivos))
+        # escuchar dispositivos
+        self.__servidorDispositivos.listen(self.nroMaxDispositivos)
+        # iniciar Server
+        print "Servidor iniciado"
+        self.aceptar_Dispositivos()
+
+    def aceptar_Dispositivos(self):
+        """
+            funcion para aceptar la conexion de cualquier dispositivo
+        """
+        while 1:
+            # aceptar la conexion de cualquier dispositivo
+            (sc, addr) = self.__servidorDispositivos.accept()
+            print "cliente conectado: ",addr
+            self.iniciar(sc)
+            self.start()
+
+    """
+        funcion para cerrar el servidor y sus conexiones
+    """
+    def cerrar_Servidor(self):
+        self.__servidorDispositivos.close()
+
+
+if __name__ == "__main__":
+    s = None
+    try:
+        s = Servidor()
+    finally:
+        if s is not None:
+            s.cerrar_Servidor()
--- prueva.py
+++
@@ -1,4 +0,0 @@
-Clemente
-========
-
-Proyecto en fedora.
